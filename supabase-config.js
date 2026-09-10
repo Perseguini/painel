c816d165e87694e644e87c60bf1acc148d7015b2
Guilherme <marcosperseguini1991@gmail.com>
2026-09-10 18:58:46 -0300

    edit

--- supabase-config.js
+++ supabase-config.js
@@ -4,5 +4,5 @@
 //   SUPABASE_URL      -> campo "Project URL"
 //   SUPABASE_ANON_KEY -> campo "anon" / "public" em "Project API keys"
 // ====================================================================
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA-CHAVE-ANON-PUBLICA-AQUI";
+const SUPABASE_URL = "https://lnbjbhqsqdhkhwsuecjx.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuYmpiaHFzcWRoa2h3c3VlY2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNzIyMzksImV4cCI6MjEwNDY0ODIzOX0.y5ceh307RtU6E1LR1DUPW8_p5f1kvmWz6Jh0ZeL6i50";
